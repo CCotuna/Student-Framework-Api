cd6db924f201c8f5ae22b16c71df581562299a4c
Calin <cotuna.calin@yahoo.com>
2025-05-09 16:57:24 +0300

    Initial project setup

---
+++ src/main/java/com/example/studentframeworkapi/StudentFrameworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentframeworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFrameworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentFrameworkApiApplication.class, args);
+    }
+
+}
